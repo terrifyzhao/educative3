10230f5890d2f929fcdde40a804f23b8235d3cc8
joe <zjiuzhou@gmail.com>
2020-10-09 09:50:22 +0800

    add code

---
+++ 14_k_way_merge/1.py
@@ -0,0 +1,37 @@
+from __future__ import print_function
+from heapq import *
+
+
+class ListNode:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def merge_lists(lists):
+    resultHead = None
+    # TODO: Write your code here
+    return resultHead
+
+
+def main():
+    l1 = ListNode(2)
+    l1.next = ListNode(6)
+    l1.next.next = ListNode(8)
+
+    l2 = ListNode(3)
+    l2.next = ListNode(6)
+    l2.next.next = ListNode(7)
+
+    l3 = ListNode(1)
+    l3.next = ListNode(3)
+    l3.next.next = ListNode(4)
+
+    result = merge_lists([l1, l2, l3])
+    print("Here are the elements form the merged list: ", end='')
+    while result != None:
+        print(str(result.value) + " ", end='')
+        result = result.next
+
+
+main()
